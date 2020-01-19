28a8f907935a03db1054a5c829b14f8ef3583e29
flywind <xzsflywind@gmail.com>
2020-01-19 09:55:20 +0800

    testclient

--- src/prologue/testclient.nim
+++ src/prologue/testclient.nim
@@ -1,13 +1,11 @@
 import httpclient, asyncdispatch, strformat
 
 
-# proc test*(address = "127.0.0.1", port = Port(8080), route = "") {.async.} = 
-#   var client = newAsyncHttpClient()
-#   echo await client.getContent(fmt"{address}:{port.int}{route}")
+proc test*(address = "127.0.0.1", port = Port(8080), route = "") {.async.} = 
+  var client = newAsyncHttpClient()
+  echo await client.getContent(fmt"http://{address}:{port.int}{route}")
 
 when isMainModule:
-  # waitFor test()
-  # waitFor test(route = "/hello")
-  # waitFor test(route = "/home")
-  var client = newHttpClient()
-  echo client.getContent("127.0.0.1:8080/hello")
+  waitFor test()
+  waitFor test(route = "/hello")
+  waitFor test(route = "/home")
