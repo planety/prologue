638213f48c8164f4416fabba7d87a03b01a9825e
flywind <xzsflywind@gmail.com>
2020-02-12 18:49:17 +0800

    fixes

--- tests/test_application.nim
+++ tests/test_application.nim
@@ -6,35 +6,40 @@ import unittest
 
 when defined(windows):
   if not existsFile("start_server.exe"):
-    discard execProcess("nim c --hints:off start_server.nim")
+    let code = execCmd("nim c --hints:off tests/start_server.nim")
+    if code != 0:
+      raise newException(IOError, "can't compile tests/start_server.nim")
+  let process = startProcess("start_server")
 else:
   if not existsFile("start_server"):
-    discard execProcess("nim c --hints:off start_server.nim")
+    let code = execCmd("nim c --hints:off tests/start_server.nim")
+    if code != 0:
+      raise newException(IOError, "can't compile tests/start_server.nim")
+  let process = startProcess("./start_server")
 
 
 suite "Test Application":
-  let 
-    process = startProcess("start_server")
+  let
     client = newAsyncHttpClient()
     address = "127.0.0.1"
     port = Port(8080)
 
   test "can get /":
-    let 
+    let
       route = "/"
       response = waitFor client.get(fmt"http://{address}:{port}{route}")
     check response.code == Http200
     check (waitFor response.body) == "<h1>Home</h1>"
-  
+
   test "can get /hello":
-    let 
+    let
       route = "/hello"
       response = waitFor client.get(fmt"http://{address}:{port}{route}")
     check response.code == Http200
     check (waitFor response.body) == "<h1>Hello, Prologue!</h1>"
 
   test "can get /home":
-    let 
+    let
       route = "/home"
       response = waitFor client.get(fmt"http://{address}:{port}{route}")
     check response.code == Http200
