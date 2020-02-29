14f4e4d95cb6ddfd9ed75c825f383be28b5ed38c
flywind <xzsflywind@gmail.com>
2020-02-29 21:31:00 +0800

    improve csrf

--- src/prologue/middlewares/csrf.nim
+++ src/prologue/middlewares/csrf.nim
@@ -12,33 +12,44 @@ when not defined(production):
 
 
 const
-  DefaultTokenName = "CSRFToken"
+  DefaultTokenName* = "CSRFToken"
+  DefaultSecretSize* = 32
+  DefaultTokenSize* = 64
 
 
-proc generateToken*(size = DefaultEntropy): string =
+proc generateToken(secret: openArray[byte]): string =
   let  
-    secret = randomBytesSeq(size)
-    tokenSize = size * 2
+    secret = randomBytesSeq(DefaultSecretSize)
   
   var
-    mask = randomBytesSeq(size)
-    token = newSeq[byte](tokenSize)
+    mask = randomBytesSeq(DefaultSecretSize)
+    token = newSeq[byte](DefaultTokenSize)
 
-  for idx in size ..< tokenSize:
+  for idx in DefaultSecretSize ..< DefaultTokenSize:
     token[idx] = mask[idx] + secret[idx]
 
-  token[0 ..< size] = move mask
+  token[0 ..< DefaultSecretSize] = move mask
 
   result = token.urlsafeBase64Encode
 
 proc recoverToken(token: string): seq[byte] =
   let
     token = token.urlsafeBase64Decode
-    secretSize = token.len div 2
   
-  result = newSeq[byte](secretSize)
-  for idx in 0 ..< secretSize:
-    result[idx] = byte(token[idx]) - byte(token[secretSize + idx])
+  result = newSeq[byte](DefaultSecretSize)
+  for idx in 0 ..< DefaultSecretSize:
+    result[idx] = byte(token[idx]) - byte(token[DefaultSecretSize + idx])
+
+proc getToken*(ctx: Context): string =
+  if not ctx.attributes.hasKey("CSRF_COOKIE"):
+    let secret = randomBytesSeq(DefaultSecretSize)
+    result = generateToken(secret)
+    ctx.attributes["CSRF_COOKIE"] = result
+  else:
+    let secret = recoverToken(ctx.attributes["CSRF_COOKIE"])
+    result = generateToken(secret)
+  ctx.attributes["CSRF_COOKIE_USED"] = "true"
+
 
 proc checkToken*(checked, secret: string): bool =
   let
@@ -49,7 +60,7 @@ proc checkToken*(checked, secret: string): bool =
   
 proc csrfToken*(ctx: Context, size = DefaultEntropy,
     tokenName = DefaultTokenName): string {.inline.} =
-  input(`type` = "hidden", name = tokenName, value = randomString(size))
+  input(`type` = "hidden", name = tokenName, value = "")
 
 proc getToken*(ctx: Context, tokenName = DefaultTokenName): string {.inline.} =
   ctx.getCookie(tokenName)
@@ -68,9 +79,16 @@ proc CsrfMiddleWare*(tokenName = DefaultTokenName): HandlerAsync =
       await switch(ctx)
       return
 
-    if not checkToken(ctx.request.postParams[tokenName], ctx.getToken(tokenName)):
+    if not ctx.request.postParams.hasKey(tokenName):
       reject(ctx)
       return
+
+    if ctx.getToken(tokenName).len != 0:
+      if not checkToken(ctx.request.postParams[tokenName], ctx.getToken(tokenName)):
+        reject(ctx)
+        return
+    else:
+      ctx.setToken(ctx.request.postParams[tokenName])
 
     await switch(ctx)
 
