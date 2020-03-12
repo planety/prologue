f00f12ac64b6b7223e681d47117bc1cd829f1d5a
flywind <xzsflywind@gmail.com>
2020-03-12 12:54:02 +0800

    isNumeric isFloat is Bool

--- tests/test_core/test_types.nim
+++ tests/test_core/test_types.nim
@@ -1,9 +1,36 @@
-from ../../src/prologue/core/types import SecretKey, parseValue, `$`, parseStringTable
+from ../../src/prologue/core/types import SecretKey, parseValue, `$`, 
+    parseStringTable, isInt, isNumeric, isBool
 
 import unittest, strtabs
 
 
-suite "Test Types":
+suite "Test Is Utils":
+  test "isInt can work":
+    check isInt("12")
+    check isInt("-753")
+    check isInt("0")
+    check not isInt("912.6")
+    check not isInt("a912")
+  
+  test "isNumeric can work":
+    check isNumeric("12")
+    check isNumeric("-753")
+    check isNumeric("0")
+    check isNumeric("0.5")
+    check isNumeric("-912.6")
+    check not isNumeric("a912")
+    check not isNumeric("0.91.2")
+
+  test "isBool can work":
+    check isBool("true")
+    check isBool("1")
+    check isBool("yes")
+    check isBool("n")
+    check isBool("False")
+    check isBool("Off")
+
+
+suite "Test Parse Utils":
   test "can parse int":
     check:
       parseValue("12", 3) == 12
@@ -22,6 +49,8 @@ suite "Test Types":
   test "can parse string":
     check parseValue("fight", "") == "fight"
 
+
+suite "Test Secret Key":
   test "can hide secret key":
     let secretKey = SecretKey("PrologueSecretKey")
     check $secretKey == "SecretKey(********)"
