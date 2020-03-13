73169e9b6ecee585cbaf529b027713e22dbefe12
flywind <xzsflywind@gmail.com>
2020-03-13 23:26:40 +0800

    again

--- tests/test_compile/test_compile.nim
+++ tests/test_compile/test_compile.nim
@@ -6,19 +6,21 @@ import unittest
 suite "Test Examples":
   let
     helloWorldDir = "./examples/helloworld"
-    todoListDir = "./examples/todoList"
+    todoListDir = "./examples/todolist"
     blogDir = "./examples/blog"
     app = "app.nim"
-  echo getCurrentDir()
+
   test "helloworld can compile":
     let (o, errC) = execCmdEx(fmt"nim c --hints:off {helloWorldDir / app}")
     echo o
     check errC == 0
 
   test "todolist can compile":
-    let (_, errC) = execCmdEx(fmt"nim c --hints:off {todoListDir / app}")
+    let (o, errC) = execCmdEx(fmt"nim c --hints:off {todoListDir / app}")
+    echo o
     check errC == 0
 
   test "blog can compile":
-    let (_, errC) = execCmdEx(fmt"nim c --hints:off {blogDir / app}")
+    let (o, errC) = execCmdEx(fmt"nim c --hints:off {blogDir / app}")
+    echo o
     check errC == 0
