620b262e59781b6328c20bb1676150323029762c
flywind <xzsflywind@gmail.com>
2020-03-19 22:34:14 +0800

    fix

--- tests/test_utils/test_utils.nim
+++ tests/test_utils/test_utils.nim
@@ -7,20 +7,20 @@ import unittest
 suite "Tesst Utils":
   test "isStaticFile can work":
     let
-      s1 = isStaticFile("static/css/basic.css", @["templates", "static"])
-      s2 = isStaticFile("/static/css/basic.css", @["templates", "static"])
-      s3 = isStaticFile("/templates/basic.html", @["templates", "static"])
-      s4 = isStaticFile("/temp/basic.html", @["templates", "static"])
+      s1 = isStaticFile("tests/test_utils/static/css/basic.css", @["static", "tests"])
+      s2 = isStaticFile("tests/test_utils/static/css/basic.css", @["tests"])
+      s3 = isStaticFile("tests/test_utils/templates/basic.html", @["templates", "tests"])
+      s4 = isStaticFile("tests/test_utils/temp/basic.html", @["templates", "static"])
     check:
       s1.hasValue
       s1.filename == "basic.css"
-      s1.root == "static/css"
+      s1.root == "tests/test_utils/static/css"
       s2.hasValue
       s2.filename == "basic.css"
-      s2.root == "static/css"
+      s2.root == "tests/test_utils/static/css"
       s3.hasValue
       s3.filename == "basic.html"
-      s3.root == "templates"
+      s3.root == "tests/test_utils/templates"
       not s4.hasValue
       s4.filename.len == 0
       s4.root.len == 0
