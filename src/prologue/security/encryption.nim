5c30459ad6829c39d27efda9277a731d952884ce
teroz <terence.namusonge@gmail.com>
2020-05-28 16:02:48 +0200

    got it working perfectly asante Mungu

--- src/prologue/security/encryption.nim
+++ src/prologue/security/encryption.nim
@@ -47,16 +47,18 @@ proc encrypt(secret:SecretKey,message:cstring):cstring=
     
     cbc.clear()
     
-    result = encrypted
+    result = toHex($encrypted)
 
-proc decrypt(secret:SecretKey,encrypted:cstring,len:uint):string=
+proc decrypt(secret:SecretKey,encrypted:cstring):string=
     var 
         cbc : CBC[aes128]
+        len = (encrypted.len * 2).uint
         decrypted:cstring = newString(len)
+        enc:cstring = parseHexStr($encrypted)
     
     assert secret.string.len < 17,"aes128 key size is 128 bits or 16 bytes"
     cbc.init(secret.string.toOpenArrayByte(0, secret.string.len-1), cbcIV.toOpenArrayByte(0, cbcIV.len-1))
-    cbc.decrypt(cast[ptr byte](encrypted), cast[ptr byte]( decrypted), len)
+    cbc.decrypt(cast[ptr byte](enc), cast[ptr byte]( decrypted), len)
     cbc.clear()
     result = $decrypted
   
@@ -64,13 +66,14 @@ proc decrypt(secret:SecretKey,encrypted:cstring,len:uint):string=
 when isMainModule:
     let
         key:string = "1234123412ABCDEF"
-        message: cstring="""hello Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" #eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaeca
-                            t cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+        message ="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" #eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaeca 
+                            # t cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""#.strip(true,true,{'\r','\n', '\r','\n', '\f', '\v'})
 
         enc = encrypt(key.SecretKey,message)
-    echo toHex($enc).len
-    echo message.len
-    assert decrypt(key.SecretKey,enc,message.len.uint) == message
+        b64 = encode($enc)
+    #echo toHex($enc).len
+    #echo message.len
+    echo decrypt(key.SecretKey,decode(b64)) 
+    #assert decrypt(key.SecretKey,enc,message.len.uint) == message
     
