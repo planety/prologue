8db20baf8160b23baf4c95da0d780e56a6d53109
teroz <terence.namusonge@gmail.com>
2020-06-02 16:28:54 +0200

    added encryption to signed cookie

--- src/prologue/security/encryption.nim
+++ src/prologue/security/encryption.nim
@@ -1,63 +1,35 @@
 
-import nimcrypto,strutils,base64
-
-from ../core/types import SecretKey,len
-
-let cbcIV = "08090A0B0C0D0E0F" #iv: string = "ABCDEF1234123412"
+import nimcrypto,strutils
 
 
 
-
-
-
-# proc oneShot(message:cstring)=
-
-
-#     var cbc,dbc : CBC[aes128]
-#     var mlen = message.len.uint + 1
-#     var decrypted:cstring = newString(mlen)
-#     var encrypted:cstring = newString(mlen)
-
-#     cbc.init(key.toOpenArrayByte(0, key.len-1), iv.toOpenArrayByte(0, iv.len-1))
-#     cbc.encrypt(cast[ptr byte]( message), cast[ptr byte]( encrypted), mlen)
-#     echo encode($encrypted)
-#     cbc.clear()
-    
-#     echo encrypted.len
-#     ## Initialization of context one more time
-#     cbc.init(key.toOpenArrayByte(0, key.len-1), iv.toOpenArrayByte(0, iv.len-1))
-#     cbc.decrypt(cast[ptr byte](encrypted), cast[ptr byte]( decrypted), mlen)
-#     echo decrypted
-
-#     ## Do not forget to clear `CBC[aes128]` context
-#     cbc.clear()
-
-#     assert decrypted == message
-
-
-proc encrypt(secret:SecretKey,message:cstring):cstring=
+proc encrypt*(secret:string,salt:string,message:cstring):string=
     var 
         cbc : CBC[aes128]
+        sc = secret.substr(0,16)
+        iv = salt.substr(0,16)
         mlen = message.len.uint
         encrypted:cstring = newString(mlen)
         
-    assert secret.len < 17,"aes128 key size is 128 bits or 16 bytes"
-    cbc.init(secret.string.toOpenArrayByte(0, secret.string.len-1), cbcIV.toOpenArrayByte(0, cbcIV.len-1))
+    #TODO pad key and IV with PKCS7
+    cbc.init(sc.toOpenArrayByte(0, sc.len-1), iv.toOpenArrayByte(0, iv.len-1))
     cbc.encrypt(cast[ptr byte]( message), cast[ptr byte]( encrypted), mlen)
     
     cbc.clear()
     
     result = toHex($encrypted)
 
-proc decrypt(secret:SecretKey,encrypted:cstring):string=
+proc decrypt*(secret:string,salt:string,encrypted:cstring):string=
     var 
         cbc : CBC[aes128]
+        sc = secret.substr(0,16)
+        iv = salt.substr(0,16)
         len = (encrypted.len * 2).uint
         decrypted:cstring = newString(len)
         enc:cstring = parseHexStr($encrypted)
     
-    assert secret.string.len < 17,"aes128 key size is 128 bits or 16 bytes"
-    cbc.init(secret.string.toOpenArrayByte(0, secret.string.len-1), cbcIV.toOpenArrayByte(0, cbcIV.len-1))
+    #TODO pad key and IV with PKCS7
+    cbc.init(sc.toOpenArrayByte(0, sc.len-1), iv.toOpenArrayByte(0, iv.len-1))
     cbc.decrypt(cast[ptr byte](enc), cast[ptr byte]( decrypted), len)
     cbc.clear()
     result = $decrypted
@@ -65,15 +37,14 @@ proc decrypt(secret:SecretKey,encrypted:cstring):string=
     
 when isMainModule:
     let
-        key:string = "1234123412ABCDEF"
+        key = "1234123412ABCDEF"
+        salt = "08090A0B0C0D0E0F"
+
         message ="""Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" #eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaeca 
                             # t cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""#.strip(true,true,{'\r','\n', '\r','\n', '\f', '\v'})
 
-        enc = encrypt(key.SecretKey,message)
-        b64 = encode($enc)
-    #echo toHex($enc).len
-    #echo message.len
-    echo decrypt(key.SecretKey,decode(b64)) 
-    #assert decrypt(key.SecretKey,enc,message.len.uint) == message
+        enc = encrypt(key,salt,message)
+        dec = decrypt(key,salt,enc) 
+    assert dec == message
     
