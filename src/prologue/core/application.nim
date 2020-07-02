100d1ae4099bd6c8c58e79ab77c3876de2ac1b75
bung87 <crc32@qq.com>
2020-07-02 14:30:05 +0800

    add route dsl

--- src/prologue/core/application.nim
+++ src/prologue/core/application.nim
@@ -3,7 +3,7 @@ import tables, strutils, strformat, logging, strtabs, options, json
 from nativesockets import Port, `$`
 import cookiejar
 from cgi import CgiError
-
+import macros
 
 from ./utils import isStaticFile
 from ./route import pattern, initPath, initRePath, newPathHandler, newRouter,
@@ -280,6 +280,70 @@ proc run*(app: Prologue) =
     else:
       event.syncHandler()
 
+proc collectMethods(node:NimNode, o:var NimNode) {.compileTime.} = 
+  for n in node:
+    if n.kind == nnkIdent and n.repr in ["get","post","put","delete","head","options"]:
+      o.add newIdentNode( "Http" & capitalizeAscii n.repr)
+    else:
+      collectMethods(n,o)
+
+proc collectCall(node:NimNode, o:var NimNode) {.compileTime.} = 
+  for n in node:
+    if n.kind == nnkCall:
+      o.add n
+    else:
+      collectCall(n,o)
+
+proc getHandler(node:NimNode):NimNode {.compileTime.} = 
+  for n in node:
+    if n.kind == nnkIdent and n.repr notin ["get","post","put","delete","head","options"]:
+      result = n
+      break;
+    else:
+      result = getHandler(n)
+
+proc getPath(node:NimNode):NimNode {.compileTime.} = 
+  for n in node:
+    if n.kind == nnkStrLit or n.kind == nnkCallStrLit:
+      result = n
+      break;
+    else:
+      result = getPath(n)
+
+macro route*(app: Prologue, routes: untyped): untyped =
+  result = nnkStmtList.newTree()
+  for element in routes:
+    expectKind element, nnkCommand 
+    var mets = nnkBracket.newTree()
+    collectMethods(element,mets)
+    var methods = nnkPrefix.newTree(
+      newIdentNode("@"),
+      mets
+    )
+    var calls = nnkBracket.newTree()
+    collectCall(element,calls)
+    var mids = nnkExprEqExpr.newTree(
+      newIdentNode("middlewares"),
+      nnkPrefix.newTree(
+        newIdentNode("@"),
+        calls
+      )
+    )
+    let path = getPath element 
+    let handler = getHandler element 
+    var theCall = nnkCall.newTree(
+      nnkDotExpr.newTree(
+        app,
+        newIdentNode("addRoute")
+      ),
+      path,
+      handler,
+      methods,
+    )
+    if calls.len > 0:
+      theCall.add mids
+    result.add theCall
+    
 
 when isMainModule:
   proc hello*(ctx: Context) {.async.} =
@@ -308,6 +372,13 @@ when isMainModule:
 
   let settings = newSettings(appName = "StarLight", debug = true)
   var app = newApp(settings = settings)
+  # import ../middlewares/middlewares
+  # app.route:
+  #   get "/" home
+  #   get post "/hello" hello
+  #   post "/login"  do_login  debugRequestMiddleware()
+  #   get re"/post(?P<num>[\d]+)" home
+  app.addRoute("/hello", hello, @[HttpGet, HttpPost])
   app.addRoute("/", home, HttpGet)
   app.addRoute("/", home, HttpPost)
   app.addRoute("/home", home, HttpGet)
