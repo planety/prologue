4360d48935488b79a67a139a67480cfad8f6a843
flywind <xzsflywind@gmail.com>
2020-08-07 21:17:25 +0800

    minor

--- src/prologue/core/request.nim
+++ src/prologue/core/request.nim
@@ -1,15 +1,15 @@
 import asyncdispatch
 
 
-when compiles(getGlobalDispatcher().handles):
-  when defined(usestd):
-    import naive/request
-  else:
-    import beast/request
+# when compiles(getGlobalDispatcher().handles):
+#   when defined(usestd):
+#     import naive/request
+#   else:
+#     import beast/request
+# else:
+when defined(windows) or defined(usestd):
+  import naive/request
 else:
-  when defined(windows) or defined(usestd):
-    import naive/request
-  else:
-    import beast/request
+  import beast/request
 
 export request
