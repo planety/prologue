1ff7e57179404acf3a58d206f6bf4870c895739f
flywind <xzsflywind@gmail.com>
2020-09-25 10:44:56 +0800

    test

--- docs.nim
+++
@@ -1,7 +0,0 @@
-import os
-
-
-removeFile("docs/coreapi/dochack.js")
-removeFile("docs/plugin/dochack.js")
-copyFile("dochack/dochack.js", "docs/coreapi/dochack.js")
-copyFile("dochack/dochack.js", "docs/plugin/dochack.js")
