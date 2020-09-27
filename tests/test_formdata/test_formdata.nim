9e5ada7fbf21d60d1a5fba1379902bd86c0d19ca
David Krause <krause@biochem2.uni-frankfurt.de>
2020-09-27 17:41:22 +0200

    make test joinable; use base64ed mime message to avoid newline mangling
    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- tests/test_formdata/test_formdata.nim
+++ tests/test_formdata/test_formdata.nim
@@ -1,18 +1,7 @@
-discard """
-  cmd:      "nim c -r --styleCheck:hint --panics:on $options $file"
-  matrix:   "--gc:arc; --gc:arc --d:release"
-  targets:  "c"
-  nimout:   ""
-  action:   "run"
-  exitcode: 0
-  timeout:  60.0
-"""
-
 import ../../src/prologue/core/form
-import tables, os, strutils
-
-let testmime = open(getAppDir() / "testmime.txt", fmRead).readAll()
-let testfile = open(getAppDir() / "testfile.txt", fmRead).readAll()
+import tables, strutils, base64
+let testfile = open("tests/test_formdata/testfile.txt", fmRead).readAll()
+let testmime = open("tests/test_formdata/testmime.base64", fmRead).readAll().decode()
 let contenttype = "multipart/form-data; boundary=---------------------------203238422770489380538321896"
 let formPart = parseFormPart(testmime, contenttype)
 doAssert formPart.data["upload"].body.len == testfile.len
