68fc6b81af19e5e683300dfab93e8838a7f79e6f
flywind <xzsflywind@gmail.com>
2020-09-27 23:30:30 +0800

    mocking test works

--- src/prologue/mocking/mocking.nim
+++ src/prologue/mocking/mocking.nim
@@ -4,11 +4,17 @@ import ../core/application
 proc mockingMiddleware*(): HandlerAsync =
   result = proc(ctx: Context) {.async.} =
     ctx.handled = true
+    await switch(ctx)
 
 proc mockApp*(app: Prologue) =
   ## Adds mocking middleware to global middlewares.
   app.middlewares.add mockingMiddleware()
 
-proc runOnce*(app: Prologue, request: Request) =
+proc runOnce*(app: Prologue, request: Request): Context =
   ## Starts an Application.
-  waitFor handleContext(app, request)
+  result = newContext(request, initResponse(HttpVer11, Http200), app.gScope)
+  waitFor handleContext(app, result)
+
+proc runOnce*(app: Prologue, ctx: Context) =
+  ## Starts an Application.
+  waitFor handleContext(app, ctx)
---
+++ tests/mocking/t_simple.nim
@@ -0,0 +1,31 @@
+import ../../src/prologue
+import ../../src/prologue/mocking/mocking
+
+import uri
+
+
+proc hello*(ctx: Context) {.async.} =
+  resp "<h1>Hello, Prologue!</h1>"
+
+
+let settings = newSettings(debug = true)
+var app = newApp(settings = settings)
+app.addRoute("/", hello)
+mockApp(app)
+
+
+let url = parseUri("/")
+let req = initMockingRequest(
+  httpMethod = HttpGet,
+  headers = newHttpHeaders(),
+  url = url,
+  cookies = initCookieJar(),
+  postParams = newStringTable(),
+  queryParams = newStringTable(),
+  formParams = initFormPart(),
+  pathParams = newStringTable()
+)
+let ctx = runOnce(app, req)
+echo req.url
+echo ctx.response
+echo ctx.response.body
