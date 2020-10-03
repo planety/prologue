9f3d48c272d955b031a18ec9a78ac36e299625a1
David Krause <krause@biochem2.uni-frankfurt.de>
2020-10-03 17:24:35 +0200

    made test_formdata human readable
    Signed-off-by: David Krause <krause@biochem2.uni-frankfurt.de>

--- tests/test_formdata/test_formdata.nim
+++ tests/test_formdata/test_formdata.nim
@@ -1,8 +1,23 @@
 import ../../src/prologue/core/form
-import tables, strutils, base64
-let testfile = "MTIzNA0KNTY3OA0KYWJjZA==".decode()
-let testmime = open("tests/test_formdata/testmime.base64", fmRead).readAll().decode()
-let contenttype = "multipart/form-data; boundary=---------------------------203238422770489380538321896"
+import tables, strutils
+const testmime =
+  "-----------------------------263701891623491983764541468\13\10" &
+  "Content-Disposition: form-data; name=\"howLongValid\"\13\10" &
+  "\13\10" &
+  "3600\13\10" &
+  "-----------------------------263701891623491983764541468\13\10" &
+  "Content-Disposition: form-data; name=\"upload\"; filename=\"testfile.txt\"\13\10" &
+  "Content-Type: text/plain\13\10" &
+  "\13\10" &
+  "1234\13\10" &
+  "5678\13\10" &
+  "abcd\13\10" &
+  "-----------------------------263701891623491983764541468--\13\10"
+const testfile =
+  "1234\13\10" &
+  "5678\13\10" &
+  "abcd"
+let contenttype = "multipart/form-data; boundary=---------------------------263701891623491983764541468"
 let formPart = parseFormPart(testmime, contenttype)
 doAssert formPart.data["upload"].body.len == testfile.len
 doAssert formPart.data["upload"].body == testfile
