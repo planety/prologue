102388f48269894cb33b9d9a756c380f06733e61
flywind <xzsflywind@gmail.com>
2020-10-05 22:01:57 +0800

    fix test

--- tests/unit/tunit_core/tunit_utils/tunit_utils.nim
+++ tests/unit/tunit_core/tunit_utils/tunit_utils.nim
@@ -7,20 +7,20 @@ block:
   # "isStaticFile can work"
   block:
     let
-      s1 = isStaticFile("tests/unit/test_core/test_utils/static/css/basic.css", @["static", "tests"])
-      s2 = isStaticFile("tests/unit/test_core/test_utils/static/css/basic.css", @["tests"])
-      s3 = isStaticFile("tests/unit/test_core/test_utils/templates/basic.html", @["templates", "tests"])
-      s4 = isStaticFile("tests/unit/test_core/test_utils/temp/basic.html", @["templates", "static"])
+      s1 = isStaticFile("tests/unit/tunit_core/tunit_utils/static/css/basic.css", @["static", "tests"])
+      s2 = isStaticFile("tests/unit/tunit_core/tunit_utils/static/css/basic.css", @["tests"])
+      s3 = isStaticFile("tests/unit/tunit_core/tunit_utils/templates/basic.html", @["templates", "tests"])
+      s4 = isStaticFile("tests/unit/tunit_core/tunit_utils/temp/basic.html", @["templates", "static"])
 
     doAssert s1.hasValue
     doAssert s1.filename == "basic.css"
-    doAssert s1.dir == normalizedPath("tests/unit/test_core/test_utils/static/css")
+    doAssert s1.dir == normalizedPath("tests/unit/tunit_core/tunit_utils/static/css")
     doAssert s2.hasValue
     doAssert s2.filename == "basic.css"
-    doAssert s2.dir == normalizedPath("tests/unit/test_core/test_utils/static/css")
+    doAssert s2.dir == normalizedPath("tests/unit/tunit_core/tunit_utils/static/css")
     doAssert s3.hasValue
     doAssert s3.filename == "basic.html"
-    doAssert s3.dir == normalizedPath("tests/unit/test_core/test_utils/templates")
+    doAssert s3.dir == normalizedPath("tests/unit/tunit_core/tunit_utils/templates")
     doAssert not s4.hasValue
     doAssert s4.filename.len == 0
     doAssert s4.dir.len == 0
@@ -30,20 +30,20 @@ block:
   # "isStaticFile can work"
   block:
     let
-      s1 = isStaticFile("/tests//unit/test_core/test_utils////static/////css////basic.css", @["/static", "tests"])
-      s2 = isStaticFile("///////////tests/unit/test_core/test_utils/static/css/basic.css", @["tests"])
-      s3 = isStaticFile("//tests/unit/test_core/test_utils/templates///////basic.html", @["//templates", "tests"])
-      s4 = isStaticFile("tests/unit/test_core/test_utils/temp/basic.html", @["templates", "static"])
+      s1 = isStaticFile("/tests//unit/tunit_core/tunit_utils////static/////css////basic.css", @["/static", "tests"])
+      s2 = isStaticFile("///////////tests/unit/tunit_core/tunit_utils/static/css/basic.css", @["tests"])
+      s3 = isStaticFile("//tests/unit/tunit_core/tunit_utils/templates///////basic.html", @["//templates", "tests"])
+      s4 = isStaticFile("tests/unit/tunit_core/tunit_utils/temp/basic.html", @["templates", "static"])
 
     doAssert s1.hasValue
     doAssert s1.filename == "basic.css"
-    doAssert s1.dir == normalizedPath("tests/unit/test_core/test_utils/static/css")
+    doAssert s1.dir == normalizedPath("tests/unit/tunit_core/tunit_utils/static/css")
     doAssert s2.hasValue
     doAssert s2.filename == "basic.css"
-    doAssert s2.dir == normalizedPath("tests/unit/test_core/test_utils/static/css")
+    doAssert s2.dir == normalizedPath("tests/unit/tunit_core/tunit_utils/static/css")
     doAssert s3.hasValue
     doAssert s3.filename == "basic.html"
-    doAssert s3.dir == normalizedPath("tests/unit/test_core/test_utils/templates")
+    doAssert s3.dir == normalizedPath("tests/unit/tunit_core/tunit_utils/templates")
     doAssert not s4.hasValue
     doAssert s4.filename.len == 0
     doAssert s4.dir.len == 0
