31dbca6a51be2e80a75bccf343f5dbce767cac67
flywind <xzsflywind@gmail.com>
2020-10-21 16:42:24 +0800

    yaml

---
+++ tests/local/extend_config_yaml/app.nim
@@ -0,0 +1,13 @@
+import ../../../src/prologue
+import yaml
+
+
+proc yaml2json(configPath: string): JsonNode =
+  result = yaml.loadToJson(readFile(configPath))[0]
+
+proc hello(ctx: Context) {.async.} =
+  resp "Hello"
+
+var app = newAppQueryEnv(Yaml, yaml2json)
+app.get("/", hello)
+app.run()
