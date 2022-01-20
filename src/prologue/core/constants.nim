17c89553b9109f17c61b43dbd32e040a499d5beb
flywind <xzsflywind@gmail.com>
2022-01-20 19:21:43 +0800

    bump 0.5.4

--- src/prologue/core/constants.nim
+++ src/prologue/core/constants.nim
@@ -1,4 +1,4 @@
 const
-  PrologueVersion* = "0.5.2"            ## The current version of Prologue.
+  PrologueVersion* = "0.5.4"            ## The current version of Prologue.
   ProloguePrefix* = "PROLOGUE"          ## The helper prefix for environment variables.
   useAsyncHTTPServer* = defined(windows) or defined(usestd) ## Uses `asynchttpserver`.
