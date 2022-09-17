76a8503306b987d9a8e8313d5219689779847249
Philipp Doerner <philipp3000doerner@web.de>
2022-09-17 14:41:51 +0200

    REFACTOR formatting

--- examples/basic/app.nim
+++ examples/basic/app.nim
@@ -7,11 +7,12 @@ import myctx
 
 let
   env = loadPrologueEnv(".env")
-  settings = newSettings(appName = env.getOrDefault("appName", "Prologue"),
-                         debug = env.getOrDefault("debug", true),
-                         port = Port(env.getOrDefault("port", 8080)),
-                         secretKey = env.getOrDefault("secretKey", "")
-    )
+  settings = newSettings(
+    appName = env.getOrDefault("appName", "Prologue"),
+    debug = env.getOrDefault("debug", true),
+    port = Port(env.getOrDefault("port", 8080)),
+    secretKey = env.getOrDefault("secretKey", "")
+  )
 
 var app = newApp(settings = settings)
 app.use(staticFileMiddleware(env.get("staticDir")))
